9fc2fbd79dfc191614ef59601e7f6c65e2e264ba
marcelinoMSR <marcelino-sanchez2011@hotmail.com>
2023-03-14 21:27:19 -0600

    agregando gitignore para mlx

--- logistico.m
+++ logistico.m
@@ -1,5 +1,5 @@
 function [fx] = logistico(vectoy)
-%global dataYear dataPop n
+global dataYear dataPop n
 
 k=vectoy(1);
 r=vectoy(2);
@@ -7,7 +7,7 @@ r=vectoy(2);
 vecto = dataYear - 1950*ones(n,1); %ajustar los años
 P = k ./ ( 1 + exp(-r*(vecto)) .* ((k/dataYear(1)) - 1) ); % calcula P(t)
 
-u = P' - dataPop; % create a vector of the differences
+u = P - dataPop; % create a vector of the differences
 fx = u' * u; % Sum of squares of the differences between "P" and "y"
 
 end
