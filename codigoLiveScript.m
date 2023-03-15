b31bcd6897d6a2a8c6bf4285129777df5ebeee5a
marcelinoMSR <marcelino-sanchez2011@hotmail.com>
2023-03-14 21:27:29 -0600

    agregando gitignore para mlx2

---
+++ codigoLiveScript.m
@@ -0,0 +1,14 @@
+% Modelo Logístico% 
+china = load('chinalimpio.mat');
+dataYear = table2array(china.china(:, 2));
+dataPop = table2array(china.china(:, 3));
+
+% Perform element-wise division on the third column of the 'china' table
+dataPop = dataPop / 1e+9;
+
+n = length(dataYear); % get the length of t vector
+
+
+x=[1 1]';
+
+[xf, j] = desnewton(rosenbrock, x);
