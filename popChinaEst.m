3f0fc812e7649d588370035c006da9ca31844e45
marcelinoMSR <marcelino-sanchez2011@hotmail.com>
2023-03-15 18:21:05 -0600

    Casi terminado

--- popChinaEst.m
+++ popChinaEst.m
@@ -2,6 +2,6 @@ function [fx] = popChinaEst(k,r)
 
 global dataPop n
 
-vecto = linspace(1950,2100,151) - 1950*ones(n,1); %ajustar los años
+vecto = (1950:2110)' - 1950*ones(161,1); %ajustar los años
 fx = k ./ ( 1 + exp(-r*(vecto)) .* ((k/dataPop(1)) - 1) ); % calcula P(t)
 end
